ab1c62a081a08271f46fb24960fbf462958544a2
Ivan Kuchin <ivan.kuchin@xing.com>
2015-06-05 15:21:12 +0200

    copy Gemfile.lock to Mygemfile.lock if it is newer or later doesn't exist

--- etc/rbenv.d/exec/rbenv-mygemfile.bash
+++ etc/rbenv.d/exec/rbenv-mygemfile.bash
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
 if [ -z "$BUNDLE_GEMFILE" ] && [ -f Mygemfile ]; then
+  if [ ! -e Mygemfile.lock ] || [ Gemfile.lock -nt Mygemfile.lock ]; then
+    cp Gemfile.lock Mygemfile.lock
+  fi
+
   export BUNDLE_GEMFILE=Mygemfile
 fi
